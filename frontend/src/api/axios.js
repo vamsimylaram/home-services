f6f7390a46f2d4570578d90b559e0da9cfcf4200
vamsi <166479653+vamsimylaram@users.noreply.github.com>
2026-02-11 05:32:02 +0000

    Delete frontend directory

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true
-});
-
-export default api;
